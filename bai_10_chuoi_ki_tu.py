0f31615f3d59876fd9f440578a2bce8eddf6e159
Minhchau224 <lnmchau.24174600067@sv.uneti.edu.vn>
2024-12-20 12:00:52 +0700

    baitap

--- bai_10_chuoi_ki_tu.py
+++ bai_10_chuoi_ki_tu.py
@@ -1,7 +1,26 @@
-input_string = input("Nhập vào chuỗi ký tự: ")
+danh_sach_mat_hang = []
 
-chu_so = ''.join(char for char in input_string if char.isdigit())  
-chu_cai = ''.join(char for char in input_string if not char.isdigit())
+def nhap_thong_tin_mat_hang():         
+    ma_hang = input("Nhập mã hàng: ")  # kiểu chuỗi
+    ten_hang = input("Nhập tên hàng: ")  # kiểu chuỗi    bb  bbh0000000000000000000000000
+    don_vi_tinh = input("Nhập đơn vị tính: ")  # kiểu chuỗi
+    don_gia = float(input("Nhập đơn giá: "))  # kiểu số thực
+    so_luong = int(input("Nhập số lượng: "))  # kiểu số nguyên
 
-cong_tong = chu_so + chu_cai  
-print(f"Chuỗi mới sau khi dồn số sang bên trái: {cong_tong}")
+    thanh_tien = don_gia * so_luong
+    
+    ty_le_vat = 10
+    thue_vat = thanh_tien * ty_le_vat / 100
+
+    mat_hang = {
+        'Mã hàng': ma_hang,
+        'Tên hàng': ten_hang,
+        'Đơn vị tính': don_vi_tinh,
+        'Đơn giá': don_gia,
+        'Số lượng': so_luong,
+        'Thành tiền': thanh_tien,
+        'Thuế VAT': thue_vat
+    }
+    danh_sach_mat_hang.append(mat_hang)
+    print("Thông tin mặt hàng đã được thêm vào danh sách.")
+
